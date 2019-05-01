87bf24501a548826164c24d91ed4e7a6543c93bd
Kirt Morgan <kirtmorgan@live.com>
2019-05-01 13:31:32 +0100

    Turned the SQL connection into a class and methods, performing further practice

--- nw_products.py
+++ nw_products.py
@@ -1,1 +1,20 @@
 import pyodbc
+class Nwproducts:
+    def __init__(self):
+        self.server = 'localhost,1433'
+        self.database = 'Northwind'
+        self.username = 'SA'
+        self.password  = 'Passw0rd2018'
+        self.docker_db_instance = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL SERVER};SERVER='+self.server+';DATABASE='+self.database+';UID='+self.username+';PWD='+self.password)
+        self.cursor = self.docker_db_instance.cursor()
+
+    def sql_query_no_transaction(self, sql_query):
+        return self.cursor.execute(sql_query)
+
+    def print_all_product_records(self):
+        query_records = self.sql_query_no_transaction("SELECT * FROM Products")
+        while True:
+            record = query_records.fetchone()
+            if record is None:
+                break
+            print(record)
---
+++ nw_runner.py
@@ -0,0 +1,3 @@
+from nw_products import Nwproducts
+sql_runner = Nwproducts()
+sql_runner.print_all_product_records()
