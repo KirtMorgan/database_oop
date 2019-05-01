518a4e5b898c889a9c08e12ff9dd7ae59908eca1
Kirt Morgan <kirtmorgan@live.com>
2019-05-01 14:11:39 +0100

    Added a method within a class to get the average price of all products within a database, also used some encapsulation to make sure the user can only run methods and not the  method that directly interacts with the database and executes SQL

--- nw_products.py
+++ nw_products.py
@@ -1,20 +1,32 @@
 import pyodbc
+
+
 class Nwproducts:
     def __init__(self):
         self.server = 'localhost,1433'
         self.database = 'Northwind'
         self.username = 'SA'
-        self.password  = 'Passw0rd2018'
+        self.password = 'Passw0rd2018'
         self.docker_db_instance = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL SERVER};SERVER='+self.server+';DATABASE='+self.database+';UID='+self.username+';PWD='+self.password)
         self.cursor = self.docker_db_instance.cursor()
 
-    def sql_query_no_transaction(self, sql_query):
+    def __sql_query_no_transaction(self, sql_query):
         return self.cursor.execute(sql_query)
 
     def print_all_product_records(self):
-        query_records = self.sql_query_no_transaction("SELECT * FROM Products")
+        query_records = self.__sql_query_no_transaction("SELECT * FROM Products")
         while True:
             record = query_records.fetchone()
             if record is None:
                 break
             print(record)
+
+    def print_average_unit_price(self):
+        query_records = self.__sql_query_no_transaction("SELECT * FROM Products")
+        total_unit_price = []
+        while True:
+            record = query_records.fetchone()
+            if record is None:
+                break
+            total_unit_price.append(record.UnitPrice)
+        print('The Average unit price for all products is =', (sum(total_unit_price)/len(total_unit_price)))
--- nw_runner.py
+++ nw_runner.py
@@ -1,3 +1,4 @@
 from nw_products import Nwproducts
 sql_runner = Nwproducts()
-sql_runner.print_all_product_records()
+# sql_runner.print_all_product_records()
+sql_runner.print_average_unit_price()
